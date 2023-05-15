cb13e88ec859d05d38ea56cce3d8acd3ce3a1b34
Efthymis Papageorgiou <papageorgiou.efthymis@protonmail.com>
2023-05-15 21:36:52 +0300

    feat: a particle is able to receive some force and change its position/velocity

--- Barnes-Hut-Java/Particle.java
+++ Barnes-Hut-Java/Particle.java
@@ -3,6 +3,8 @@ public class Particle {
     private double y;
     private double vx;
     private double vy;
+    private double Fx;
+    private double Fy;
     private double mass;
     private String name;
 
@@ -21,11 +23,37 @@ public class Particle {
     public void setName(String name){this.name = name;}
 
     public Particle(double x, double y, double vx, double vy, double mass, String name) {
-    this.x    = x;
-    this.y    = y;
-    this.vx   = vx;
-    this.vy   = vy;
-    this.mass = mass;
-    this.name = name;
+        this.name = name;
+        this.x    = x;
+        this.y    = y;
+        this.vx   = vx;
+        this.vy   = vy;
+        this.mass = mass;
+        this.Fx   = 0;
+        this.Fy   = 0;
+    }
+
+    public double distanceToParticle(Particle p) {
+        double dx = this.x-p.x;
+        double dy = this.y-p.y;
+        double d  = Math.sqrt(dx*dx+dy*dy);
+        return d;
+    }
+
+    public void applyForce(Particle b) {
+        double dx  = b.x - this.x;
+        double dy  = b.y - this.y;
+        double dis = Math.sqrt(dx*dx + dy*dy);
+        double G   = 6.67e-11;
+        double F   = (G * this.mass * b.mass) / (dis*dis);
+        this.Fx    = F * dx / dis;
+        this.Fy    = F * dy / dis;
+    }
+
+    public void updatePositionAndVelocity(double dt) {
+        this.x  += dt * this.vx;
+        this.y  += dt * this.vy;
+        this.vx += dt * this.Fx / this.mass;
+        this.vy += dt * this.Fy / this.mass;
     }
 }
